bc0320992e5ed96f7c10fae722fe33c0207c3116
feneboy3 <dankala2007@gmail.com>
2026-05-31 23:51:09 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
 // Copyright 2021 NNTU-CS
-#include <iostream>
 #include <string>
+
 #include "bst.h"
 
-void makeTree(BST<std::string>& tree, const char* filename);
-void printFreq(BST<std::string>& tree);
+void makeTree(BST<std::string>& tree, const char* filename);  // NOLINT
+void printFreq(BST<std::string>& tree);  // NOLINT
 
 int main() {
     BST<std::string> tree;
